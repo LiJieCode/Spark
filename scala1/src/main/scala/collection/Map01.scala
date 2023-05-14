9dc0ee2b01883dbf69ef3c083e2d6c9fed785c44
l9zyr <l9zyr95@163.com>
2023-05-14 11:04:09 +0800

    0514-master-1

--- scala1/src/main/scala/collection/Map01.scala
+++ scala1/src/main/scala/collection/Map01.scala
@@ -2,16 +2,9 @@ package collection
 
 object Map01 {
     def main(args: Array[String]): Unit = {
-
         val map = Map(1 -> "zhang1", 2 -> "zhang2")
-
         val str1: String = map.mkString(",")
-
         println(str1)
-        
-        // 这里我写一点东西
-
         println("li")
-
     }
 }
