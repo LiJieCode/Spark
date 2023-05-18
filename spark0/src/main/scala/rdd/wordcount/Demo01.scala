f76323296ced7c1fcedf9470e2d561ec6abf4283
l9zyr <l9zyr95@163.com>
2023-05-18 22:49:52 +0800

    0518

--- spark0/src/main/scala/rdd/wordcount/Demo01.scala
+++ spark0/src/main/scala/rdd/wordcount/Demo01.scala
@@ -11,16 +11,29 @@ object Demo01 {
 
         // val wc: RDD[(String, Int)] = sc.textFile("data/1.txt").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
 
-        val wc: RDD[(String, Int)] = sc.textFile("data/1.txt").flatMap(_.split(" ")).groupBy(s => s).map {
+//        val wc: RDD[(String, Int)] = sc.textFile("data/1.txt").flatMap(_.split(" ")).groupBy(s => s).map {
+//            case (word, list) => {
+//                (word, list.size)
+//            }
+//        }
+
+        // val wc: RDD[(String, Int)] = sc.textFile("").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+
+        val rdd: RDD[String] = sc.textFile("")
+
+        val mapRDD: RDD[String] = rdd.flatMap(_.split(" "))
+
+        val groupRDD: RDD[(String, Iterable[String])] = mapRDD.groupBy(s => s)
+
+        val wc: RDD[(String, Int)] = groupRDD.map {
             case (word, list) => {
                 (word, list.size)
             }
         }
+
         wc.foreach(println)
 
         sc.stop()
-
-
 
     }
 
